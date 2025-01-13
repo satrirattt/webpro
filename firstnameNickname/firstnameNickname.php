8619d20348b75524db55c31fe97245ff39dff03b
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2025-01-13 14:32:17 +0700

    Update firstnameNickname.php

--- firstnameNickname/firstnameNickname.php
+++ firstnameNickname/firstnameNickname.php
@@ -9,68 +9,81 @@
         <?php
         if (isset($_POST['submit'])) {
             $filename = $_POST['filename'];
+            if (file_exists($filename)) {
+                
+            
             $text = file($filename);
+            echo "<table border='0' cellpadding='0' cellspacing='0'>";
             foreach ($text as $tr_data) {
+                echo "<tr>"; 
                 $col = 1;
                 $array_word = explode(",", $tr_data);
 
                 foreach ($array_word as $value) {
                     $value = trim($value);
                     if ($col == 1) {
-                        echo $value;
+                        echo "<td width = '150'><br>$value</td>";
                     } else {
                         if ($value == "Robert")
-                            echo "Dick";
+                            echo "<td width = '150'><br>Dick</td>";
                         elseif ($value == "Dick")
-                            echo "Robert";
+                            echo "<td width = '150'><br>Robert</td>";
                         if ($value == "William")
-                            echo "Bill";
+                            echo "<td width = '150'><br>Bill</td>";
                         elseif ($value == "Bill")
-                            echo "William";
+                            echo "<td width = '150'><br>William</td>";
                         if ($value == "James")
-                            echo "Jim";
+                            echo "<td width = '150'><br>Jim</td>";
                         elseif ($value == "Jim")
-                            echo "James";
+                            echo "<td width = '150'><br>James</td>";
                         if ($value == "John")
-                            echo "Jack";
+                            echo "<td width = '150'><br>Jack</td>";
                         elseif ($value == "Jack")
-                            echo "John";
+                            echo "<td width = '150'><br>John</td>";
                         if ($value == "Margaret")
-                            echo "Peggy";
+                            echo "<td width = '150'><br>Peggy</td>";
                         elseif ($value == "Peggy")
-                            echo "Margaret";
+                            echo "<td width = '150'><br>Margaret</td>";
                         if ($value == "Edward")
-                            echo "Ed";
+                            echo "<td width = '150'><br>Ed</td>";
                         elseif ($value == "Ed")
-                            echo "Edward";
+                            echo "<td width = '150'><br>Edward</td>";
                         if ($value == "Sarah")
-                            echo "Sally";
+                            echo "<td width = '150'><br>Sally</td>";
                         elseif ($value == "Sally")
-                            echo "Sarah";
+                            echo "<td width = '150'><br>Sarah</td>";
                         if ($value == "Andrew")
-                            echo "Andy";
+                            echo "<td width = '150'><br>Andy</td>";
                         elseif ($value == "Andy")
-                            echo "Andrew";
+                            echo "<td width = '150'><br>Andrew</td>";
                         if ($value == "Anthony")
-                            echo "Tony";
+                            echo "<td width = '150'><br>Tony</td>";
                         elseif ($value == "Tony")
-                            echo "Anthony";
+                            echo "<td width = '150'><br>Anthony</td>";
                         if ($value == "Deborah")
-                            echo "Debbie";
+                            echo "<td width = '150'><br>Debbie</td>";
                         elseif ($value == "Debbie")
-                            echo "Deborah";
+                            echo "<td width = '150'><br>Deborah</td>";
+
+                           
                     }
                     $col++;
                 }
+                
+                
             }
+            
+        } else {
+            echo "<p style='color:red;'>File not found. Please check the file name.</p>";
+        }
         } else {
         ?>
             <h3>firstnameNickname.php</h3>
             <form method="post" action="firstnameNickname.php">
                 <table border="0" align="center" width="400">
                     <tr>
-                        <td width="100">File name </td>
-                        <td width="100" align="center"><input type="text" name="filename" size="40" value=""></td>
+                        <td width="150">File name </td>
+                        <td width="150" align="center"><input type="text" name="filename" size="40" value=""></td>
                     </tr>
                     <tr>
                     <tr></tr>
